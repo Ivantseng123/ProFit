ab09d7edca906eb125f0f277b85bc33037d03e6b
Ivantseng123 <as82858285@gmail.com>
2024-09-07 19:25:16 +0800

    完成CourseLessonDao

--- src/main/java/com/ProFit/dao/coursesCRUD/HcourseLessonDao.java
+++ src/main/java/com/ProFit/dao/coursesCRUD/HcourseLessonDao.java
@@ -3,6 +3,7 @@ package com.ProFit.dao.coursesCRUD;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import com.ProFit.bean.coursesBean.CourseLessonBean;
 
@@ -14,34 +15,86 @@ public class HcourseLessonDao implements IHcourseLessonDao {
 		this.session = session;
 	}
 	
+	//新增章節單元
 	@Override
 	public CourseLessonBean insertCourseLesson(CourseLessonBean courseLesson) {
+		if(courseLesson!=null) {
+			session.persist(courseLesson);
+			return courseLesson;
+		}
 		return null;
 	}
 
+	//刪除章節單元By id
 	@Override
 	public boolean deleteCourseLessonByID(String courseLessonId) {
+		CourseLessonBean courseLessonBean = session.get(CourseLessonBean.class, courseLessonId);
+		if(courseLessonBean!=null) {
+			session.remove(courseLessonBean);
+			return true;
+		}
 		return false;
 	}
-
+	
+	//更新課程單元
 	@Override
 	public boolean updateCourseLessonById(CourseLessonBean newCourseLesson) {
-		return false;
+		CourseLessonBean oldCourseLesson = session.get(CourseLessonBean.class, newCourseLesson.getCourseLessonId());
+		
+		if(oldCourseLesson==null) {
+			System.out.println("Lesson does not exist");
+			return false;
+		}
+		
+		//對比新舊物件的屬性
+		oldCourseLesson.setCourseLessonName(
+				newCourseLesson.getCourseLessonName() == null || newCourseLesson.getCourseLessonName().isEmpty()
+					?oldCourseLesson.getCourseLessonName()
+					:newCourseLesson.getCourseLessonName());
+		
+		oldCourseLesson.setCourseLessonSort(
+				newCourseLesson.getCourseLessonSort() == null || newCourseLesson.getCourseLessonSort().isEmpty()
+					?oldCourseLesson.getCourseLessonSort()
+					:newCourseLesson.getCourseLessonSort());
+		
+		oldCourseLesson.setLessonMediaType(
+				newCourseLesson.getLessonMediaType() == null || newCourseLesson.getLessonMediaType().isEmpty()
+					?oldCourseLesson.getLessonMediaType()
+					:newCourseLesson.getLessonMediaType());
+		
+		oldCourseLesson.setLessonMediaUrl(
+				newCourseLesson.getLessonMediaUrl() == null || newCourseLesson.getLessonMediaUrl().isEmpty()
+					?oldCourseLesson.getLessonMediaUrl()
+					:newCourseLesson.getLessonMediaUrl());
+		
+		oldCourseLesson.setMediaDuration(
+				newCourseLesson.getMediaDuration() ==null || newCourseLesson.getMediaDuration().isEmpty()
+					?oldCourseLesson.getMediaDuration()
+					:newCourseLesson.getMediaDuration());
+		
+		session.merge(oldCourseLesson);
+		return true;
 	}
-
+	
+	//搜尋單個單元by id
 	@Override
 	public CourseLessonBean searchOneCourseLessonById(int courseLessonId) {
 		return session.get(CourseLessonBean.class, courseLessonId);
 	}
 
+	//搜尋全部單元
 	@Override
 	public List<CourseLessonBean> searchCourseLessons() {
-		return null;
+		Query<CourseLessonBean> query = session.createQuery("from CourseLessonBean",CourseLessonBean.class);
+		return query.list();
 	}
 
+	//搜尋全部單元by courseModule id
 	@Override
 	public List<CourseLessonBean> searchCourseLessonsByModuleId(int courseModuleid) {
-		return null;
+		Query<CourseLessonBean> query = session.createQuery("from CourseLessonBean where courseModule.courseModuleId = :courseModuleId",CourseLessonBean.class);
+		query.setParameter("courseModuleId", courseModuleid);
+		return query.getResultList();
 	}
 
 }
--- src/main/java/com/ProFit/dao/coursesCRUD/HcourseModuleDao.java
+++ src/main/java/com/ProFit/dao/coursesCRUD/HcourseModuleDao.java
@@ -11,73 +11,74 @@ import com.ProFit.bean.coursesBean.CourseModuleBean;
 import jakarta.persistence.Id;
 
 public class HcourseModuleDao implements IHcourseModuleDao {
-	
+
 	private Session session;
-	
+
 	public HcourseModuleDao(Session session) {
 		this.session = session;
 	}
-	
-	//新增課程章節
+
+	// 新增課程章節
 	@Override
 	public CourseModuleBean insertCourseModule(CourseModuleBean courseModule) {
-		if(courseModule!=null) {
+		if (courseModule != null) {
 			session.persist(courseModule);
 			return courseModule;
 		}
 		return null;
 	}
-	
-	//刪除課程章節By id
+
+	// 刪除課程章節By id
 	@Override
 	public boolean deleteCourseModuleById(String courseModuleId) {
 		CourseModuleBean courseModuleBean = session.get(CourseModuleBean.class, courseModuleId);
-		if(courseModuleBean!=null) {
+		if (courseModuleBean != null) {
 			session.remove(courseModuleBean);
 			return true;
 		}
 		return false;
 	}
-	
-	//更新課程章節
+
+	// 更新課程章節
 	@Override
 	public boolean updateCourseModuleById(CourseModuleBean newCourseModule) {
 		CourseModuleBean oldCourseModule = session.get(CourseModuleBean.class, newCourseModule.getCourseModuleId());
-		
-		if(oldCourseModule==null) {
+
+		if (oldCourseModule == null) {
 			System.out.println("Module does not exist");
 			return false;
 		}
-		
-		//對比新舊物件的屬性
+
+		// 對比新舊物件的屬性
 		oldCourseModule.setCourseModuleName(
-			newCourseModule.getCourseModuleName() ==null || newCourseModule.getCourseModuleName().isEmpty()?
-			oldCourseModule.getCourseModuleName():newCourseModule.getCourseModuleName()
-		);
-		
-		session.merge(newCourseModule);
+				newCourseModule.getCourseModuleName() == null || newCourseModule.getCourseModuleName().isEmpty()
+						? oldCourseModule.getCourseModuleName()
+						: newCourseModule.getCourseModuleName());
+
+		session.merge(oldCourseModule);
 		return true;
 	}
-	
-	//搜尋課程章節by id
+
+	// 搜尋課程章節by id
 	@Override
 	public CourseModuleBean searchOneCourseModuleById(int courseModuleId) {
 		return session.get(CourseModuleBean.class, courseModuleId);
 	}
-	
-	//搜尋全部課程章節
+
+	// 搜尋全部課程章節
 	@Override
 	public List<CourseModuleBean> searchCourseModules() {
-		Query<CourseModuleBean> query = session.createQuery("from course_module",CourseModuleBean.class);
+		Query<CourseModuleBean> query = session.createQuery("from CourseModuleBean", CourseModuleBean.class);
 		return query.list();
 	}
-	
-	//搜尋課程章節by courseId
+
+	// 搜尋課程章節by courseId
 	@Override
 	public List<CourseModuleBean> searchCourseModules(String course_id) {
-	    Query<CourseModuleBean> query = session.createQuery("from CourseModuleBean where course.courseId = :courseid", CourseModuleBean.class);
-	    query.setParameter("courseid", course_id); // 使用 setParameter 設定參數的值
-	    return query.getResultList(); // 執行查詢並返回結果
+		Query<CourseModuleBean> query = session.createQuery("from CourseModuleBean where course.courseId = :courseid",
+				CourseModuleBean.class);
+		query.setParameter("courseid", course_id); // 使用 setParameter 設定參數的值
+		return query.getResultList(); // 執行查詢並返回結果
 	}
 
 }
